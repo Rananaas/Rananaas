aba5fec691d1e76460d46ac889bb83cedd73be9f
Rananaas <ranascheerman@gmail.com>
2020-11-20 11:57:20 +0100

    updatePages

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const poefButton = document.querySelector('#poef');
+let poefAan = false;
+
+poefButton.addEventListener('click', function () {
+
+    if(!poefAan){
+        console.log("in paf");
+        poefButton.innerText = "PAF";
+        poefAan =true;
+    } else if(poefAan){ 
+        console.log("in poef");
+        poefButton.innerText ="POEF";
+        poefAan = false;
+    }
+
+ })

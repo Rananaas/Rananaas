af383105a82d4dcadd2d4c0e6b635a2c72ef77c8
Rananaas <ranascheerman@gmail.com>
2021-01-30 18:13:07 +0100

    update30januari

--- spel.js
+++ spel.js
@@ -11,4 +11,10 @@ $('#spel7Modal').on('hidden.bs.modal', function () {
     $('#movieinmodal7f')[0].pause();
     $('#movieinmodal7g')[0].pause();
     $('#movieinmodal7h')[0].pause();
-})
+});
+
+$('spel2Modal').on('hidden.bs.modal', function(){ 
+    $('#movieinmodal2a')[0].pause();
+    $('#movieinmodal2b')[0].pause();
+    $('#movieinmodal2c')[0].pause()
+});
